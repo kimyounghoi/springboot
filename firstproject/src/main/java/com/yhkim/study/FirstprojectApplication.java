9618fee90ca8615b5d40930cc84cc6b0904b36dc
yhkim <yhkim@DESKTOP-YHKIM>
2018-08-28 10:40:35 +0900

    creaate spring boot project

---
+++ firstproject/src/main/java/com/yhkim/study/FirstprojectApplication.java
@@ -0,0 +1,12 @@
+package com.yhkim.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+}
